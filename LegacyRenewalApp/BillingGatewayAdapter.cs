2bddba0df5faa6c4c676f6ceb3010a68fddc4692
Karina Szczepanek <s26646@pjwstk.edu.pl>
2026-04-11 22:55:44 +0200

    commit

---
+++ LegacyRenewalApp/BillingGatewayAdapter.cs
@@ -0,0 +1,10 @@
+﻿namespace LegacyRenewalApp;
+
+public class BillingGatewayAdapter : IBillingGateway
+{
+    public void SaveInvoice(RenewalInvoice invoice)
+        => LegacyBillingGateway.SaveInvoice(invoice);
+
+    public void SendEmail(string email, string subject, string body)
+        => LegacyBillingGateway.SendEmail(email, subject, body);
+}
